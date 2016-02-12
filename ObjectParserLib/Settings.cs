a53bc3c90ecb0bb0b033e1d0da88ce7acc837d0b
Jason Lautzenheiser <lautzenheiser.jason@gmail.com>
2016-02-11 22:26:07 -0500

    settings test

--- ObjectParserLib/Settings.cs
+++ ObjectParserLib/Settings.cs
@@ -4,8 +4,21 @@ namespace ObjectParserLib
 {
   public class Settings
   {
-    public string ObjectDelimiter { get; private set; } = Environment.NewLine;
-    public string ChildIndicator { get; private set; } = "+";
-    public string Attributes { get; private set; } = "[*]";
+    public string ObjectDelimiter { get;  private set; } = Environment.NewLine;
+    public string ChildIndicator { get;  private set; } = "+";
+    public string Attributes { get;  private set; } = "[*]";
+
+    public Settings()
+    {
+    }
+
+    public Settings(string objectDelimiter, string childIndicator, string attributes)
+    {
+      ObjectDelimiter = objectDelimiter;
+      ChildIndicator = childIndicator;
+      Attributes = attributes;
+    }
   }
+
+
 }
---
+++ ObjectParserTests/SettingsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using ObjectParserLib;
+
+namespace ObjectParserTests
+{
+  [TestFixture]
+  public class SettingsTests
+  {
+
+    [Test]
+    public void DefaultConstructorSetsDefaultSettings()
+    {
+      var settings = new Settings();
+
+      Assert.That(settings.Attributes == "[*]" && settings.ChildIndicator == "+" && settings.ObjectDelimiter == Environment.NewLine, Is.True);
+    }
+
+    [Test]
+    public void ConstructorSetsValues()
+    {
+      var settings = new Settings("|", "-", "A");
+      Assert.That(settings.Attributes == "A" && settings.ChildIndicator == "-" && settings.ObjectDelimiter == "|", Is.True);
+    }
+  }
+}
